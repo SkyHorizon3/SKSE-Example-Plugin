073e5031ac670450ad9f6b1ce9c20a24940e0410
SkyHorizon3 <neuner.r@gmx.de>
2025-04-05 20:53:46 +0200

    remove stuff included in CLib

--- include/PCH.h
+++ include/PCH.h
@@ -62,24 +62,3 @@ namespace stl
 		T::func = reinterpret_cast<std::uintptr_t>(alloc);
 	}
 }
-
-//https://github.com/powerof3/CLibUtil/blob/master/include/CLIBUtil/singleton.hpp
-template <class T>
-class ISingleton
-{
-public:
-	static T* GetSingleton()
-	{
-		static T singleton;
-		return std::addressof(singleton);
-	}
-
-protected:
-	ISingleton() = default;
-	~ISingleton() = default;
-
-	ISingleton(const ISingleton&) = delete;
-	ISingleton(ISingleton&&) = delete;
-	ISingleton& operator=(const ISingleton&) = delete;
-	ISingleton& operator=(ISingleton&&) = delete;
-};
